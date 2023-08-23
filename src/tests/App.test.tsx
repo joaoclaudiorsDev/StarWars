e58e5f54a77963bfb62633b87e32de3d1dd7584d
João Cláudio <joaoclaudiobio@gmail.com>
2023-08-22 23:09:42 -0300

    req 5

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,9 +1,42 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from '../App';
+import { fireEvent, render } from '@testing-library/react';
+import PlanetsApiWithProvider from '../components/PlanetsApiWithProvider';
+import PlanetsApi from '../components/PlanetsApi';
 
-test('I am your test', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders PlanetsApiWithProvider component', () => {
+  const { getByText } = render(<PlanetsApiWithProvider />);
+  
+  const headerElement = getByText(/Star Wars Planets/i);
+  expect(headerElement).toBeInTheDocument();
+});
+
+test('renders all data-testid elements in PlanetsApi', () => {
+  const { getByTestId } = render(<PlanetsApi />);
+
+  const nameFilterElement = getByTestId('name-filter');
+  const columnFilterElement = getByTestId('column-filter');
+  const comparisonFilterElement = getByTestId('comparison-filter');
+  const valueFilterElement = getByTestId('value-filter');
+  const buttonFilterElement = getByTestId('button-filter');
+  
+  expect(nameFilterElement).toBeInTheDocument();
+  expect(columnFilterElement).toBeInTheDocument();
+  expect(comparisonFilterElement).toBeInTheDocument();
+  expect(valueFilterElement).toBeInTheDocument();
+  expect(buttonFilterElement).toBeInTheDocument();
+});
+
+test('verifies that numeric filter starts with 0', () => {
+  const { getByTestId } = render(<PlanetsApi />);
+  
+  const selectElement = getByTestId('column-filter');
+  const comparisonElement = getByTestId('comparison-filter');
+  const valueElement = getByTestId('value-filter');
+  
+  fireEvent.change(selectElement, { target: { value: 'population' } });
+  
+  fireEvent.change(comparisonElement, { target: { value: 'maior que' } });
+  
+  const numericValue = 0;
+  expect(valueElement).toHaveValue(numericValue);
 });
